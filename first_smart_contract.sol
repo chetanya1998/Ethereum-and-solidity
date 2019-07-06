dc2d52adb2c1b3a0434d5827ed2075696013836c
chetanya1998 <31440658+chetanya1998@users.noreply.github.com>
2019-07-07 04:52:04 +0530

    initial commit

---
+++ first_smart_contract.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.4.17;
+
+contract inbox{
+    string public message;
+    function  inbox(string initialMesage)public{
+        message = initialMesage;
+        
+    }
+    function setMesage(string newMessage)public{
+        message = newMessage;
+    }
+    function getmessage() public view returns (string){
+        return message;
+    }
+}
